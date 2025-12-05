0e2c23fcafef29f01c6151082647246b837ac912
alda-98 <aldabravo.98@outlook.com>
2025-12-04 19:35:05 -0600

    cambios

--- app_movil_escolar_api/settings.py
+++ app_movil_escolar_api/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '-_&+lsebec(whhw!%n@ww&1j=4-^j_if9x8$q
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # Agregar el host de Render en producción
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
@@ -46,8 +46,7 @@ MIDDLEWARE = [
 
 # Configuración de CORS: define orígenes permitidos
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4200',
-    'https://app-movil-escolar-webapp.onrender.com',
+    '*',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
